765883c25a2d03340a52000f8ee5ac945538fc8c
TimWidow <eat.twix@gmail.com>
2021-05-04 21:35:36 +0300

    matrix

--- prototype.py
+++ prototype.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from copy import deepcopy
+from copy import copy, deepcopy
 
 
 class Prototype(ABC):
@@ -9,26 +9,77 @@ class Prototype(ABC):
 
 
 class ConcretePrototype1(Prototype):
-    def __init__(self, arg1, arg2):
-        self.field1 = arg1
-        self.field2 = arg2
+    def __init__(self, arg):
+        self.field = arg
 
     def __operation__(self):
         self.performed_operation = True
 
-    def clone(self):
-        obj = ConcretePrototype1(self.field1, self.field2)
-        obj.performed_operation = self.performed_operation
-        return obj
+    def clone(self) -> object:
+        return copy(self)
 
 
 class ConcretePrototype2(Prototype):
-    def __init__(self, arg1, arg2):
-        self.field1 = arg1
-        self.field2 = arg2
+    def __init__(self, arg):
+        self.field = arg
 
     def __operation__(self):
         self.performed_operation = True
 
     def clone(self):
         return deepcopy(self)
+
+
+if __name__ == "__main__":
+    p1 = ConcretePrototype1([1, 2, 3, 4, 5])
+    print("Client: Object created ", p1.field, "id =", id(p1))
+
+    p2 = p1.clone()
+    print("Client: Object copy    ", p2.field, "id =", id(p2))
+
+    p2.field[0] = 7
+    p2.field.pop()
+    p2.field.append(7)
+
+    print()
+
+    print("Client: Original list  ", p1.field, "id =", id(p1))
+    print("Client: List copy      ", p2.field, "id =", id(p2))
+
+    print()
+
+    if p1.field is p2.field:
+        print("Objects is the same")
+
+    if id(p1.field) == id(p2.field):
+        print("Prototype copy works, both variables depends each other.")
+    else:
+        print("Prototype failed, variables contain different instances.")
+
+    print()
+
+    p_1 = ConcretePrototype2([1, 2, 3, 4, 5])
+    print("Client: Object created ", p_1.field, "id =", id(p_1))
+
+    p_2 = p1.clone()
+    print("Client: Object deepcopy", p_2.field, "id =", id(p_2))
+
+    p_2.field[0] = 7
+    p_2.field.pop()
+    p_2.field.append(7)
+
+    print()
+
+    print("Client: Original list  ", p_1.field, "id =", id(p_1))
+    print("Client: List deepcopy  ", p_2.field, "id =", id(p_2))
+
+    print()
+
+    if p_1.field is p_2.field:
+        print("Objects is the same")
+
+    if id(p_1.field) == id(p_2.field):
+        print("Prototype deepcopy failed, both variables depends each other.")
+    else:
+        print("Prototype deepcopy works, variables contain different instances.")
+
--- singleton.py
+++ singleton.py
@@ -6,18 +6,18 @@ class Singleton:
         return cls.instance
 
 
-s1 = Singleton()
-print("Object created", s1, "id =", id(s1))
+if __name__ == "__main__":
+    s1 = Singleton()
+    print("Object created", s1, "id =", id(s1))
+    # Singleton.instance = '<__main__.Singleton object at 0x0000027671C5DF70>'
 
-# Singleton.instance = '<__main__.Singleton object at 0x0000027671C5DF70>'
+    s2 = Singleton()
+    print("Object created", s2, "id =", id(s2))
 
-s2 = Singleton()
-print("Object created", s2, "id =", id(s2))
+    if s1 is s2:
+        print("Objects is the same")
 
-if s1 is s2:
-    print("Objects is the same")
-
-if id(s1) == id(s2):
-    print("Singleton works, both variables contain the same instance.")
-else:
-    print("Singleton failed, variables contain different instances.")
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
