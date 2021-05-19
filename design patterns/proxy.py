657d33186a6a8ceba72b41a8f0bdbc0117bff118
TimWidow <Pahjkm11>
2021-05-19 16:49:11 +0300

    Proxy.drawio

--- design patterns/proxy.py
+++ design patterns/proxy.py
@@ -41,13 +41,13 @@ def client_code(subject: Subject) -> None:
 
 if __name__ == "__main__":
     print("Client: Executing the client code with a real subject:")
-    speak("Client: Executing the client code with a real subject:")
+    speak("Executing the client code with a real subject")
     real_subject = RealSubject()
     client_code(real_subject)
 
     print("")
 
     print("Client: Executing the same client code with a proxy:")
-    speak("Client: Executing the same client code with a proxy:")
+    speak("Executing the same client code with a proxy")
     proxy = Proxy(real_subject)
     client_code(proxy)
