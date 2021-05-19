33a9abc781d301a5f6c4fc9de2f5ca6486ce5769
TimWidow <Pahjkm11>
2021-05-19 17:22:50 +0300

    Visitor.drawio

--- design patterns/visitor.py
+++ design patterns/visitor.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
 
+from text_to_speech import speak
+
 
 class Component(ABC):
 
@@ -16,7 +18,7 @@ class ConcreteComponentA(Component):
         visitor.visit_concrete_component_a(self)
 
     @staticmethod
-    def exclusive_method_of_concrete_component_a() -> str:
+    def exclusive_method_a() -> str:
         return "A"
 
 
@@ -26,7 +28,7 @@ class ConcreteComponentB(Component):
         visitor.visit_concrete_component_b(self)
 
     @staticmethod
-    def special_method_of_concrete_component_b() -> str:
+    def special_method_b() -> str:
         return "B"
 
 
@@ -43,18 +45,18 @@ class Visitor(ABC):
 
 class ConcreteVisitor1(Visitor):
     def visit_concrete_component_a(self, element) -> None:
-        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor1")
+        print(f"{element.exclusive_method_a()} + ConcreteVisitor1")
 
     def visit_concrete_component_b(self, element) -> None:
-        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor1")
+        print(f"{element.special_method_b()} + ConcreteVisitor1")
 
 
 class ConcreteVisitor2(Visitor):
     def visit_concrete_component_a(self, element) -> None:
-        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor2")
+        print(f"{element.exclusive_method_a()} + ConcreteVisitor2")
 
     def visit_concrete_component_b(self, element) -> None:
-        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor2")
+        print(f"{element.special_method_b()} + ConcreteVisitor2")
 
 
 def client_code(comp: List[Component], visitor: Visitor) -> None:
@@ -66,10 +68,12 @@ def client_code(comp: List[Component], visitor: Visitor) -> None:
 if __name__ == "__main__":
     components = [ConcreteComponentA(), ConcreteComponentB()]
 
-    print("The client code works with all visitors via the base Visitor interface:")
+    print("Client: The client code works with all visitors via the base Visitor interface:")
+    speak("The client code works with all visitors via the base Visitor interface")
     visitor1 = ConcreteVisitor1()
     client_code(components, visitor1)
 
-    print("It allows the same client code to work with different types of visitors:")
+    print("Client: It allows the same client code to work with different types of visitors:")
+    speak("It allows the same client code to work with different types of visitors")
     visitor2 = ConcreteVisitor2()
     client_code(components, visitor2)
