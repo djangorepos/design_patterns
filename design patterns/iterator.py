f937a97fbd8c88d579026a1b7ce5d03aa2a61f61
TimWidow <Pahjkm11>
2021-05-26 19:09:49 +0300

    iterator.py

--- design patterns/iterator.py
+++ design patterns/iterator.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
+
+import random
 from collections.abc import Iterable, Iterator
+from string import digits, ascii_lowercase, ascii_uppercase
+from time import sleep
 from typing import Any
 from text_to_speech import speak
 
@@ -45,15 +49,38 @@ class WordsCollection(Iterable):
 
 if __name__ == "__main__":
     collection = WordsCollection()
-    collection.add_item("First")
-    collection.add_item("Second")
-    collection.add_item("Third")
+    collection.add_item("One")
+    collection.add_item("Two")
+    collection.add_item("Three")
+    collection.add_item("Four")
+    collection.add_item("Five")
+    collection.add_item("Six")
+    collection.add_item("Seven")
+    collection.add_item("Eight")
+    collection.add_item("Nine")
+    collection.add_item("Ten")
+    collection.add_item("Eleven")
+    collection.add_item("Twelve")
 
-    print("Straight traversal:")
-    speak("Straight traversal")
-    print("\n".join(collection))
+    print("Client: Checking Systems:")
+    speak("Checking Systems")
+    for _ in range(101):
+        print('\r', _, '%', end='#' * _)
+        sleep(0.01)
     print("")
+    print(" ".join(collection))
+    print("")
+    print("Client: Thirty seconds and counting. Astronauts report it feels good. T-25 seconds. Twenty seconds and "
+          "counting. T-15 seconds, guidance is internal.")
 
-    print("Reverse traversal:")
-    speak("Reverse traversal")
-    print("\n".join(collection.get_reverse_iterator()), end="")
+    reverse = collection.get_reverse_iterator()
+    for word in reverse:
+        if word == "Six":
+            print("Client: Ignition sequence start.")
+            speak("Ignition sequence start")
+        print(word)
+        speak(word)
+    print("Client: All engines running.")
+    speak("All engines running.")
+    print("Client: Liftoff!")
+    speak(" Liftoff!")
